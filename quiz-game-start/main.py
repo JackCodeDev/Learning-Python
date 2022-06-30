4aa467931e3b5e5996e1dfdbd70d727f6b5f75f3
Jack Nguyen <93038913+JackCodeDev@users.noreply.github.com>
2022-06-30 23:46:22 +0700

    Add files via upload

---
+++ quiz-game-start/main.py
@@ -0,0 +1,21 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+point = 0
+quiz = QuizBrain(question_bank)
+question = 0
+while quiz.still_has_question():
+    quiz.next_question()
+
+
+
+
+
+
