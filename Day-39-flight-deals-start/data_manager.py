1063cb0d0597e17683acd02005e7e28029dc8e3f
Jack Nguyen <93038913+JackCodeDev@users.noreply.github.com>
2022-07-26 16:37:26 +0700

    Add files via upload

---
+++ Day-39-flight-deals-start/data_manager.py
@@ -0,0 +1,38 @@
+import requests
+from pprint import pprint
+Google_Sheet_API_endpoint = "https://api.sheety.co/39fbc02e801a2073ba7218dbb94be15c/flightDeals/prices"
+
+
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=Google_Sheet_API_endpoint)
+        response.raise_for_status()
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"],
+                }
+            }
+            response = requests.put(url=f"{Google_Sheet_API_endpoint}/{city['id']}",
+                                     json=new_data)
+            response.raise_for_status()
+            print(response.text)
+
+
+data_news = DataManager()
+data_news.get_destination_data()
+data_news.update_destination_codes()
+
+
+
+
+
